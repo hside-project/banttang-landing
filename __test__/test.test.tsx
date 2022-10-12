e1d63f9687ceb71bf9f0b0e0947ea9fb2e99c52c
taehee <klimes888@gmail.com>
2022-10-12 09:07:23 +0900

    Updates

--- __test__/test.test.tsx
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-
-import { render, screen } from '@testing-library/react';
-
-test('Renders main element', () => {
-    render(<div>test</div>);
-    const mainElement = screen.getByText('test');
-    expect(mainElement).toBeInTheDocument();
-});
--- index.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './components/App';
-
-const container = document.getElementById('root');
-const root = createRoot(container as Element);
-
-root.render(<App />);
