3cc106cc8fe4da364d2b1116401c90003a018e56
summerr0-0 <summerr060@gamil.com>
2023-02-21 20:15:51 +0900

    랜딩페이지 제작

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,64 +1,82 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 // components
 
 export default function MainPage() {
-  const dom = window.innerHeight;
+    const dom = window.innerHeight;
 
-  return (
-    <Layout>
-      <LayoutInner>
-        <Page height={dom}>
-          <IntroductionArea>
-            <WrapperBox>
-              <TitleText>
-                당신 근처의 당근마켓
-              </TitleText>
-              <SubText>
-                중고 거래부터 동네 정보까지, 이웃과 함께해요.<br />
-                가깝고 따뜻한 당신의 근처를 만들어요.
-              </SubText>
-            </WrapperBox>
-          </IntroductionArea>
-          <ImageArea>
-            그림영역
-          </ImageArea>
-        </Page>
-        <SecondPage height={dom}>
-          <IntroductionArea>
-            <WrapperBox>
-              <TitleText>
-                우리 동네 중고 직거래 마켓
-              </TitleText>
-              <SubText>
-                동네 주민들과 가깝고 따뜻한 거래를 지금 경험해보세요
-              </SubText>
-            </WrapperBox>
-          </IntroductionArea>
-          <ImageArea>
-            그림영역
-          </ImageArea>
-        </SecondPage>
-        <ThirdPage height={dom}>
-          <IntroductionArea>
-            <WrapperBox>
-              <TitleText>
-                이웃과 함께 하는 동네생활
-              </TitleText>
-              <SubText>
-                우리 동네의 다양한 이야기를 이웃과 함께 나누어요.
-              </SubText>
-            </WrapperBox>
-          </IntroductionArea>
-          <ImageArea>
-            그림영역
-          </ImageArea>
-        </ThirdPage>
+    return (
+        <Layout>
+            <LayoutInner>
+                <Page height={dom} color = {"bisque"}>
+                    <IntroductionArea>
+                        <WrapperBox>
+                            <TitleText>
+                                함께 사요 반땅!
+                            </TitleText>
+                            <SubText>
+                                딱 하나만 필요할때! <br/>
+                                혼자 사기엔 너무 많은 물건들<br/>
+                                이웃들과 나눠서 구매해요<br/>
+                            </SubText>
+                        </WrapperBox>
+                    </IntroductionArea>
+                    <ImageArea>
+                        <LandingImage src="imgs/landing-img1.png" />
+                    </ImageArea>
+                </Page>
+                <Page height={dom} color={"white"}>
+                    <IntroductionArea>
+                        <WrapperBox>
+                            <TitleText>
+                                더이상 남기지 않아요!
+                            </TitleText>
+                            <SubText>
+                                혼자먹기 너무 많은 배달음식,<br/>
+                                배달비는 줄이고 음식물 쓰레기는 줄여요!<br/>
+                            </SubText>
+                        </WrapperBox>
+                    </IntroductionArea>
+                    <ImageArea>
+                        <LandingDoubleImage src="imgs/landing-img2.png" />
+                    </ImageArea>
+                </Page>
+                <Page height={dom} color = {"bisque"}>
+                    <IntroductionArea>
+                        <WrapperBox>
+                            <TitleText>
+                                가까운 사람들과 거래해요
+                            </TitleText>
+                            <SubText>
+                                내 위치 기반 가까운 글들이 보여요
+                            </SubText>
+                        </WrapperBox>
+                    </IntroductionArea>
+                    <ImageArea>
+                        <LandingImage src="imgs/landing-img3.png" />
+                    </ImageArea>
+                </Page>
+                <Page height={dom} color={"white"}>
+                    <IntroductionArea>
+                        <WrapperBox>
+                            <TitleText>
+                                반땅 함께하러 가기
+                            </TitleText>
+                            <SubText>
+                                play store 다운로드
+                                (app store 출시예정)
+                            </SubText>
+                        </WrapperBox>
+                    </IntroductionArea>
+                    <ImageArea>
+                        <QrImage src="imgs/banttang_qrcode.png"  />
+                    </ImageArea>
+                </Page>
 
-      </LayoutInner>
-    </Layout>
-  );
+            </LayoutInner>
+        </Layout>
+    );
 }
 
 const Layout = styled.div`
@@ -87,27 +105,9 @@ const Text = styled.span`
   font-weight: 700;
 `;
 
-const Page = styled.div <{ height: number}>`
-  background-color: palegoldenrod;
+const Page = styled.div <{ height: number , color: String}>`
   display: flex;
-  width: 100vw;
-  height: ${(props) => props.height + "px"};
-  justify-conent: center;
-  align-items: center;
-`;
-
-const SecondPage = styled.div <{ height: number }>`
-  background-color: cornflowerblue;
-  display: flex;
-  width: 100vw;
-  height: ${(props) => props.height + "px"};
-  justify-conent: center;
-  align-items: center;
-`;
-
-const ThirdPage = styled.div <{ height: number }>`
-  background-color: darksalmon;
-  display: flex;
+  background-color: ${(props) => props.color};
   width: 100vw;
   height: ${(props) => props.height + "px"};
   justify-conent: center;
@@ -125,7 +125,6 @@ const IntroductionArea = styled.div`
 
 const ImageArea = styled.div`
   display: flex;
-  background-color: white;
   width: 100vw;
   height: 100%;
   flex: 1;
@@ -147,5 +146,25 @@ const TitleText = styled.h1`
 
 const SubText = styled.span`
   line-height: 2em;
-
 `;
+
+const LandingImage = styled.img`
+  display: flex;
+  width: 400px;
+  margin : auto;
+  object-fit: contain;
+`
+
+const LandingDoubleImage = styled.img`
+  display: flex;
+  width: 700px;
+  margin : auto;
+  object-fit: contain;
+`
+
+const QrImage = styled.img`
+  display: flex;
+  width: 300px;
+  margin : auto;
+  object-fit: contain;
+`
